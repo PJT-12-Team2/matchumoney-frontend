7c425c525519d4421bc8f2b10e1979907508dfd8
MJ <nampink2300@gmail.com>
2025-07-21 16:13:03 +0900

    init: 보유 적금 기반 적금 추천 페이지 생성

---
+++ src/pages/savings/recommendations/History.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import SavingMyProductSlider from '@/components/savings/SavingMyProductSlider.vue';
+import BankbookProductList from '@/components/bankbook/BankbookProductList.vue';
+import RecommendationLayout from '@/components/layouts/RecommendationLayout.vue';
+// ✅ 이 부분 추가!
+const selectedId = ref(null);
+</script>
+
+<template>
+  <RecommendationLayout title="적금 추천">
+    <template #slider>
+      <SavingMyProductSlider @select="(id) => (selectedId = id)" />
+    </template>
+
+    <template #highlight> 지금보다 더 좋은 적금 상품을 찾았어요! </template>
+
+    <template #content>
+      <BankbookProductList :selectedId="selectedId" />
+    </template>
+  </RecommendationLayout>
+</template>
