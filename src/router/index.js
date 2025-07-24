9cd6d106e8378463460cf80ed73dccef4c898b27
tjdgus3488 <tjdgus3488@naver.com>
2025-07-24 16:15:58 +0900

    fix: index.js 충돌 해결

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
-<<<<<<< HEAD
 import RecommendDeposit from "@/pages/deposit/RecommendDeposit.vue";
 import SignupPage from "@/pages/auth/SignupPage.vue";
 import LoginPage from "@/pages/auth/LoginPage.vue";
-=======
 import MyPage from "@/pages/user/MyPage.vue";
 import RecommendDeposit from "@/pages/deposit/RecommendDeposit.vue";
 import UpdateUserInfoPage from "@/pages/user/UpdateUserInfoPage.vue";
 import UpdatePasswordPage from "@/pages/user/UpdatePasswordPage.vue";
->>>>>>> b785d0540aa512f444b222260e259c14cff0bb8d
 
 const routes = [
   {
@@ -18,20 +15,16 @@ const routes = [
     component: HomePage,
   },
   {
-<<<<<<< HEAD
-=======
     path: "/mypage",
     name: "mypage",
     component: MyPage,
   },
   {
->>>>>>> b785d0540aa512f444b222260e259c14cff0bb8d
     path: "/api/deposits/recommendations/history",
     name: "RecommendDeposit",
     component: RecommendDeposit,
   },
   {
-<<<<<<< HEAD
     path: "/signup",
     name: "Signup",
     component: SignupPage,
@@ -40,7 +33,6 @@ const routes = [
     path: "/login",
     name: "Login",
     component: LoginPage,
-=======
     path: "/mypage/update",
     name: "updateUserInfo",
     component: UpdateUserInfoPage,
@@ -49,7 +41,6 @@ const routes = [
     path: "/mypage/update/password",
     name: "updatePassword",
     component: UpdatePasswordPage,
->>>>>>> b785d0540aa512f444b222260e259c14cff0bb8d
   },
 ];
 
