4e790aa6b6819c2407c4e741ad12d39b1d89380f
tjdgus3488 <tjdgus3488@naver.com>
2025-07-29 15:34:11 +0900

    feat: accessToken 재발급 후 자동 적용

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 
 const instance = axios.create({
+  withCredentials: true, // 쿠키를 포함한 요청
   timeout: 300000, //30초
   //  codef 적금 불러오기 시간초과 발생 방지를 위해 길게 설정
 });
@@ -18,6 +19,34 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { response } = error;
+    const authStore = useAuthStore();
+    const originalRequest = error.config;
+
+    if (response && response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      console.log("📦 응답 헤더 전체:", response.headers);
+      const newAccessToken = response.headers["authorization"];
+      console.log("🔄 새 accessToken이 재발급되어 반영됩니다:", newAccessToken);
+
+      if (newAccessToken) {
+        const tokenOnly = newAccessToken.startsWith("Bearer ") ? newAccessToken.slice(7) : newAccessToken;
+        authStore.setToken(tokenOnly);
+        originalRequest.headers["Authorization"] = `Bearer ${tokenOnly}`;
+        return instance(originalRequest);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
