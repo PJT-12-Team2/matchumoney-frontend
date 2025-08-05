8c2c143dc9db8bd54e87e7550442e2ce411a4d92
MJ <nampink2300@gmail.com>
2025-08-05 09:15:45 +0900

    feat: 비교 페이지 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,149 +1,155 @@
 // router/index.js 또는 router.js
-import { createRouter, createWebHistory } from "vue-router";
-import KakaoCallbackPage from "@/pages/auth/KakaoCallbackPage.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import KakaoCallbackPage from '@/pages/auth/KakaoCallbackPage.vue';
 
 /* ── 페이지 컴포넌트 ─────────────────────── */
-import HomePage from "@/pages/HomePage.vue";
-import MyPage from "@/pages/user/MyPage.vue";
-import MyInfoPage from "@/pages/user/MyInfoPage.vue";
-import RecommendDeposit from "@/pages/deposit/RecommendDeposit.vue";
-import SignupPage from "@/pages/auth/SignupPage.vue";
-import LoginPage from "@/pages/auth/LoginPage.vue";
-import ResetPasswordPage from "@/pages/auth/ResetPasswordPage.vue";
-import UpdateUserInfoPage from "@/pages/user/UpdateUserInfoPage.vue";
-import UpdatePasswordPage from "@/pages/user/UpdatePasswordPage.vue";
+import HomePage from '@/pages/HomePage.vue';
+import MyPage from '@/pages/user/MyPage.vue';
+import MyInfoPage from '@/pages/user/MyInfoPage.vue';
+import RecommendDeposit from '@/pages/deposit/RecommendDeposit.vue';
+import SignupPage from '@/pages/auth/SignupPage.vue';
+import LoginPage from '@/pages/auth/LoginPage.vue';
+import ResetPasswordPage from '@/pages/auth/ResetPasswordPage.vue';
+import UpdateUserInfoPage from '@/pages/user/UpdateUserInfoPage.vue';
+import UpdatePasswordPage from '@/pages/user/UpdatePasswordPage.vue';
 
-import PersonaSurveyPage from "@/pages/persona/PersonaSurveyPage.vue";
-import PersonaSurveyStart from "@/pages/persona/PersonaSurveyStartPage.vue";
-import PersonaCardAllList from "@/pages/persona/PersonaCardAllList.vue";
-import PersonaDepositAllList from "@/pages/persona/PersonaDepositAllListPage.vue";
-import PersonaSavingAllList from "@/pages/persona/PersonaSavingAllListPage.vue";
+import PersonaSurveyPage from '@/pages/persona/PersonaSurveyPage.vue';
+import PersonaSurveyStart from '@/pages/persona/PersonaSurveyStartPage.vue';
+import PersonaCardAllList from '@/pages/persona/PersonaCardAllList.vue';
+import PersonaDepositAllList from '@/pages/persona/PersonaDepositAllListPage.vue';
+import PersonaSavingAllList from '@/pages/persona/PersonaSavingAllListPage.vue';
 
-import RecommendSavings from "@/pages/savings/recommendations/RecommendSavings.vue";
-import MyDataCardPage from "@/pages/mydata/MyDataCardPage.vue";
+import RecommendSavings from '@/pages/savings/recommendations/RecommendSavings.vue';
+import MyDataCardPage from '@/pages/mydata/MyDataCardPage.vue';
 
-import CardRecommendationPage from "@/pages/cards/CardRecommendationPage.vue";
+import CardRecommendationPage from '@/pages/cards/CardRecommendationPage.vue';
 
+import ComparePage from '@/pages/compare/ComparePage.vue';
 /* 결과 페이지 (동적 import) */
-const ResultPage = () => import("@/pages/persona/PersonaResultPage.vue");
+const ResultPage = () => import('@/pages/persona/PersonaResultPage.vue');
 
 /* 8종 페르소나 코드 */
 const personaCodes = [
-  "turtle",
-  "squirrel",
-  "ant",
-  "owl",
-  "rabbit",
-  "cat",
-  "tiger",
-  "penguin",
+  'turtle',
+  'squirrel',
+  'ant',
+  'owl',
+  'rabbit',
+  'cat',
+  'tiger',
+  'penguin',
 ];
 
 const routes = [
-  { path: "/", name: "home", component: HomePage },
-  { path: "/signup", name: "signup", component: SignupPage },
-  { path: "/login", name: "login", component: LoginPage },
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/signup', name: 'signup', component: SignupPage },
+  { path: '/login', name: 'login', component: LoginPage },
   {
-    path: "/oauth/kakao/callback",
-    name: "KakaoCallback",
+    path: '/oauth/kakao/callback',
+    name: 'KakaoCallback',
     component: KakaoCallbackPage,
   },
   {
-    path: "/reset-password",
-    name: "resetPassword",
+    path: '/reset-password',
+    name: 'resetPassword',
     component: ResetPasswordPage,
     component: KakaoCallbackPage,
   },
   {
-    path: "/reset-password",
-    name: "resetPassword",
+    path: '/reset-password',
+    name: 'resetPassword',
     component: ResetPasswordPage,
   },
   {
-    path: "/mypage",
-    name: "mypage",
+    path: '/mypage',
+    name: 'mypage',
     component: MyPage,
   },
-  { path: "/myinfo", name: "myinfo", component: MyInfoPage },
+  { path: '/myinfo', name: 'myinfo', component: MyInfoPage },
   {
-    path: "/myinfo/update",
-    name: "updateUserInfo",
+    path: '/myinfo/update',
+    name: 'updateUserInfo',
     component: UpdateUserInfoPage,
   },
   {
-    path: "/myinfo/update/password",
-    name: "updatePassword",
+    path: '/myinfo/update/password',
+    name: 'updatePassword',
     component: UpdatePasswordPage,
   },
 
   {
-    path: "/myinfo/update",
-    name: "updateUserInfo",
+    path: '/myinfo/update',
+    name: 'updateUserInfo',
     component: UpdateUserInfoPage,
   },
   {
-    path: "/myinfo/update/password",
-    name: "updatePassword",
+    path: '/myinfo/update/password',
+    name: 'updatePassword',
     component: UpdatePasswordPage,
   },
   {
-    path: "/deposits/recommendations/history",
-    name: "RecommendDeposit",
+    path: '/deposits/recommendations/history',
+    name: 'RecommendDeposit',
     component: RecommendDeposit,
   },
   {
-    path: "/savings/recommendations/history",
-    name: "RecommendSavings",
+    path: '/savings/recommendations/history',
+    name: 'RecommendSavings',
     component: RecommendSavings,
+  },
+  {
+    path: '/compare',
+    name: 'ComparePage',
+    component: ComparePage,
   },
 
   {
-    path: "/mydata/cards",
-    name: "MyDataCards",
+    path: '/mydata/cards',
+    name: 'MyDataCards',
     component: MyDataCardPage,
   },
   {
-    path: "/cards/recommendations",
-    name: "CardRecommendations",
+    path: '/cards/recommendations',
+    name: 'CardRecommendations',
     component: CardRecommendationPage,
   },
   {
-    path: "/cards/recommendations/:cardId",
-    name: "CardRecommendation",
+    path: '/cards/recommendations/:cardId',
+    name: 'CardRecommendation',
     component: CardRecommendationPage,
     props: true,
   },
   {
-    path: "/persona",
+    path: '/persona',
     children: [
-      { path: "start", name: "PersonaTest", component: PersonaSurveyStart },
-      { path: "survey", name: "PersonaSurvey", component: PersonaSurveyPage },
-      { path: "cards", name: "PersonaCards", component: PersonaCardAllList },
+      { path: 'start', name: 'PersonaTest', component: PersonaSurveyStart },
+      { path: 'survey', name: 'PersonaSurvey', component: PersonaSurveyPage },
+      { path: 'cards', name: 'PersonaCards', component: PersonaCardAllList },
       {
-        path: "savings",
-        name: "PersonaSavings",
+        path: 'savings',
+        name: 'PersonaSavings',
         component: PersonaSavingAllList,
       },
       {
-        path: "deposits",
-        name: "PersonaDeposits",
+        path: 'deposits',
+        name: 'PersonaDeposits',
         component: PersonaDepositAllList,
       },
       {
-        path: "result/:code",
-        name: "PersonaResult",
+        path: 'result/:code',
+        name: 'PersonaResult',
         component: ResultPage,
         props: true,
         beforeEnter: (to, _, next) => {
           if (personaCodes.includes(to.params.code)) next();
-          else next("/");
+          else next('/');
         },
       },
     ],
   },
-  { path: "/survey", redirect: "/persona/survey" },
-  { path: "/personatest", redirect: "/persona/test" },
-  { path: "/personaCardList", redirect: "/persona/cards" },
+  { path: '/survey', redirect: '/persona/survey' },
+  { path: '/personatest', redirect: '/persona/test' },
+  { path: '/personaCardList', redirect: '/persona/cards' },
   ...personaCodes.map((c) => ({
     path: `/${c}`,
     redirect: `/persona/result/${c}`,
